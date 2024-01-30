c5a6174f166d7920cc96503804ed010c647617e3
taktasovae <olobok251298@mail.ru>
2024-01-30 16:30:40 +0500

    add new test

--- pages/tensorpage.py
+++ pages/tensorpage.py
@@ -1,15 +1,16 @@
 from selenium.webdriver.common.by import By
 from .basepage import BasePage
 from selenium.webdriver.support import expected_conditions as EC
-
 tensor_button_locator = (By.CSS_SELECTOR, 'a.sbisru-Contacts__logo-tensor')
-block_sila_locator = (By.XPATH, '//*[@id="container"]/div[1]/div/div[5]/div/div/div[1]/div/p[1]')
-popodrobnee_locator = (By.XPATH, '//*[@id="container"]/div[1]/div/div[5]/div/div/div[1]/div/p[4]/a')
+block_sila_locator = (
+    By.XPATH, '//*[@id="container"]/div[1]/div/div[5]/div/div/div[1]/div/p[1]')
+popodrobnee_locator = (
+    By.XPATH, '//*[@id="container"]/div[1]/div/div[5]/div/div/div[1]/div/p[4]/a')
+
+
 class TensorPage(BasePage):
     def __init__(self, driver):
         super().__init__(driver)
 
     def open(self):
         self.driver.get('https://tensor.ru/')
-
-
--- test_1.py
+++ test_1.py
@@ -1,11 +1,13 @@
 import pytest
-from pages.tensorpage import TensorPage, tensor_button_locator, block_sila_locator
+from pages.tensorpage import TensorPage, tensor_button_locator, block_sila_locator, popodrobnee_locator
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from pages.contactpage import ContactsPage, contacts_buttton_locator
-driver=webdriver.Chrome()
+image_locator = (
+    By.XPATH, '//*[@id="container"]/div[1]/div/div[4]/div[2]/div[1]')
+driver = webdriver.Chrome()
 # Проверяю переход на tensor.ru через контакты и  баннер "тензор"
 # def test_tensor_banner():
 #     contacts = ContactsPage(driver)
@@ -29,43 +31,29 @@ driver=webdriver.Chrome()
 
 # Перейдите в этом блоке в "Подробнее" и убедитесь, что открывается
 # https://tensor.ru/about
-popodrobnee_locator = (By.XPATH, '//*[@id="container"]/div[1]/div/div[5]/div/div/div[1]/div/p[4]/a')
-def test_url_tensor_about():
-    popodrobnee = TensorPage(driver)
-    popodrobnee.open()
-    popodrobnee.find(popodrobnee_locator).click()
-    # WebDriverWait(popodrobnee.driver, 10).until(EC.number_of_windows_to_be(2))
-    # Получаем список идентификаторов вкладок
-    # window_handles = popodrobnee.driver.window_handles
-    # Переключаемся на последнюю открытую вкладку
-    # popodrobnee.driver.switch_to.window(window_handles[-1])
-    expected_url = "https://tensor.ru/about"
-    actual_url = popodrobnee.driver.current_url
-    assert actual_url == expected_url
-    
-#     def test_tensor_page(self):
-#         # Открываем сайт https://tensor.ru/
-#         self.driver.get('https://tensor.ru/')
 
-#         # Проверяем, что есть блок "Сила в людях"
-#         strength_in_people = WebDriverWait(self.driver, 10).until(
-#             EC.presence_of_element_located((By.ID, 'strength-in-people')))
-#         assert strength_in_people is not None
+# def test_url_tensor_about():
+#     popodrobnee_page = TensorPage(driver)
+#     popodrobnee_page.open()
+#     # скроем элемент, пока другие его перекрывают
+#     driver.execute_script(
+#         "arguments[0].style.display = 'none';", driver.find_element(By.XPATH, '//noindex'))
 
-#         # Переходим в "Подробнее" и проверяем URL
-#         details = WebDriverWait(self.driver, 10).until(
-#             EC.presence_of_element_located((By.LINK_TEXT, 'Подробнее')))
-#         details.click()
+#     popodr_button = driver.find_element(*popodrobnee_locator)
+#     # прибегнула к исп-нию JavaScript-кода, другого способа пока не нашла
+#     driver.execute_script("arguments[0].click();", popodr_button)
+#     expected_url = "https://tensor.ru/about"
+#     actual_url = driver.current_url
+#     assert actual_url == expected_url
 
-#         assert self.driver.current_url == 'https://tensor.ru/about'
 
-#         # Проверяем размеры фотографий в разделе "Работаем"
-#         images = self.driver.find_elements(
-#             By.XPATH, '//div[@id="work-with-us"]//img')
-#         heights = {image.size['height'] for image in images}
-#         widths = {image.size['width'] for image in images}
-#         assert len(heights) == 1 and len(widths) == 1
+def test_image_size():
+    pass
 
+
+# Закрытие браузера
+driver.quit()
 
 if __name__ == '__main__':
+
     pytest.main()
