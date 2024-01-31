3ef2dbc5eff0ce528932e351236f51530dbfe488
taktasovae <olobok251298@mail.ru>
2024-01-31 16:34:12 +0500

    finished second scenary

--- test_2.py
+++ test_2.py
@@ -4,20 +4,71 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from pages.contactpage import ContactsPage, contacts_buttton_locator
-# Второй сценарий:
-# 1) Перейти на https://sbis.ru/ в раздел "Контакты"
-# 2) Проверить, что определился ваш регион (в нашем примере
-# Ярославская обл.) и есть список партнеров.
-# 3) Изменить регион на Камчатский край
-# 4) Проверить, что подставился выбранный регион, список партнеров
-# изменился, url и title содержат информацию выбранного региона
+my_region_locator = (
+    By.XPATH, '//*[@id="container"]/div[1]/div/div[3]/div[2]/div[1]/div/div[2]/span/span')
+kamchatka_locator = (
+    By.XPATH, '//*[@id="container"]/div[1]/div/div[3]/div[2]/div[1]/div/div[2]/span/span')
+
+partners_locator_my_reg = (
+    By.XPATH, '//*[@id="contacts_list"]/div/div[2]/div[2]/div/div[2]/div[1]/div[3]')
+partners_locator_kamchat_list = (
+    By.XPATH, '//*[@id="contacts_list"]/div/div[2]/div[2]/div/div[2]/div[1]/div[3]')
+
+locator_kamchat_reg_inregions_list = (
+    By.XPATH, '//*[@id="popup"]/div[2]/div/div/div/div/div[2]/div/ul/li[43]/span')
+
+tyumen_partners1 = (
+    By.XPATH, "//div[@class='controls-ListViewV__itemsContainer']//div[@class='controls-ListView__itemV-relative']")
+kamchatka_partners = (
+    By.XPATH, "//div[@class='controls-ListViewV__itemsContainer']//div[@class='controls-ListView__itemV-relative']")
+
 driver = webdriver.Chrome()
+
+# проверка установления моего региона и списка партнеров по нему
+
+
 def test_my_region():
     contacts = ContactsPage(driver)
     contacts.open()
     contacts.find(contacts_buttton_locator).click()
+    my_region = contacts.find(my_region_locator)
+    partners_my_reg_list = contacts.find(partners_locator_my_reg)
+    assert partners_my_reg_list.is_displayed(
+    ), 'Список партнеров не отображается на странице'
+    assert my_region.text == 'Тюменская обл.', 'Мой регион устанавливается неверно'
+
+
+def test_kamchat_reg():
+    contacts = ContactsPage(driver)
+    contacts.open()
+    contacts.find(contacts_buttton_locator).click()
+    my_region = contacts.find(my_region_locator)
+    my_region.click()
+    # меняем регион - переходим на камчатку
+    select_kamchat_reg = WebDriverWait(driver, 10).until(
+        EC.visibility_of_element_located(locator_kamchat_reg_inregions_list))
+    select_kamchat_reg.click()
+    WebDriverWait(driver, 5).until(EC.url_contains('kamchatskij-kraj'))
+    new_url = driver.current_url
+    kamchatka_reg_is_displayed = contacts.find(kamchatka_locator)
+    title_kamchatka = kamchatka_reg_is_displayed.get_attribute('title')
+    # проверка урла и тайтл
+    assert 'kamchatskij-kraj' in new_url
+    assert title_kamchatka in kamchatka_reg_is_displayed.get_attribute(
+        "title"), "Атрибут title не содержит информацию о выбранном регионе"
+    # проверяем, что подставился выбранный регион, список партнеров(и он изменился)
+    assert kamchatka_reg_is_displayed.text == 'Камчатский край'
+
+    kamchatka_partners = driver.find_elements(
+        By.XPATH, "//div[@class='controls-ListViewV__itemsContainer']//div[@class='controls-ListView__itemV-relative']")
+    tyumen_partners = driver.find_elements(
+        By.XPATH, "//div[@class='controls-ListViewV__itemsContainer']//div[@class='controls-ListView__itemV-relative']")
+    # Сравниваем содержимое каждого элемента в списках партнеров
+    for kamchatka_partner, tyumen_partner in zip(kamchatka_partners, tyumen_partners):
+        assert kamchatka_partner.text != tyumen_partner.text, "Содержимое партнеров в разных регионах совпадает"
+
+
 # Закрытие браузера
 # driver.quit()
-
 if __name__ == '__main__':
     pytest.main()
